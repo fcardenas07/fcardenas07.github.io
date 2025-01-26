1c698b4d3ea6b3575f13d727f75f0f86beeaaf78
Francisco Cárdenas <f.cardenas07@ufromail.cl>
2025-01-26 18:41:48 -0300

    Add deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://fcardenas07.github.io',
     integrations: [
         mdx(),
         sitemap(),
